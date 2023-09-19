1a93969d980cf57579786bff15caefa10a6b66ea
gritsakan <gritsakan1133@gmail.con>
2023-09-19 15:56:31 +0700

    nine summit assign

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,16 @@
 // Exercise #1: Food Order
 
 // Start coding here
+
+let foodOrder = {
+  orderNumber: "A0234",
+  address: "Bankok",
+  paymentType: "Cash",
+  totalPrice: 4500,
+  restaurantName: "MK",
+};
+
+foodOrder.totalPrice = 5000;
+foodOrder.paymentType = "Credit Card";
+
+console.log(foodOrder);
